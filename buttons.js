e1fafae558ad855572d637bb631d5a8f9757c112
Lowkeyoj <rockyojaji@gmail.com>
2024-02-04 12:49:03 -0800

    hope this works

---
+++ buttons.js
@@ -0,0 +1,46 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the button element by its ID
+    var buttonElement = document.getElementById('About');
+
+    // Add a click event listener to the button
+    buttonElement.addEventListener('click', function () {
+        // Change the location to the desired page
+        window.location.href = 'page2.html';
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the button element by its ID
+    var buttonElement = document.getElementById('Home');
+
+    // Add a click event listener to the button
+    buttonElement.addEventListener('click', function () {
+        // Change the location to the desired page
+        window.location.href = 'page_1.html';
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the button element by its ID
+    var buttonElement = document.getElementById('Services');
+
+    // Add a click event listener to the button
+    buttonElement.addEventListener('click', function () {
+        // Change the location to the desired page
+        window.location.href = 'page3.html';
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Get the button element by its ID
+    var buttonElement = document.getElementById('Contact');
+
+    // Add a click event listener to the button
+    buttonElement.addEventListener('click', function () {
+        // Change the location to the desired page
+        window.location.href = 'contact.html';
+    });
+});
